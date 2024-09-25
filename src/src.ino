29d9dba5fe1885bd28ade2085f329bbcc3ca7763
AlexanderMiheev <sashamiheev1611@gmail.com>
2024-09-25 17:59:38 +0300

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,27 +1,48 @@
 #include "mg.h"
+Graphics gfx;
+Joystick Joy;
+void drawField() {
+  u8g2.drawHLine(0, 0, 128);
+  u8g2.drawHLine(0, 63, 128);
 
-Joystick joy;
-Graphics gtx;
-
-void readDataJoy() 
-{
-joy.updatePositionXY();
-
-  if (joy.pressKeyA() == true)
+  for (int a = 0; a <= 64; a = a + 6) 
   {
-    gtx.print(13, (String)joy.posX1 + "l" + (String)joy.posY1, 0, 13, 13, 8);
-  } 
-  else
-  {
-    gtx.print(13, "no joy", 0, 13, 13, 8);
-    u8g2.drawFrame(5, joy.posY1, 5, 15);
-  } 
+    u8g2.drawVLine(64, a, 4);
+  }
 }
+void drawRackets()
+{
+  Joy.updatePositionXY();
 
+u8g2.drawFrame(5,Joy.posY0,5,10);
+u8g2.drawFrame(117,Joy.posY1,5,10);
+}
+int xBall{63}; int yBall{31};
+void drawBall()
+{
+  int xx{2}; int yy{1}; 
+  u8g2.drawBox(xBall,yBall,4,4);
+  xBall += xx ;
+  yBall += yy ;
+if(xBall >= 128) xx = -2;
+if(xBall <= 0) xx =2;
+if(yBall>= 64) xx = -1;
+if(yBall <= 0) xx =1;
+
+
+
+
+}
+void gamePong()
+{
+  drawField();
+  drawRackets();
+  drawBall();
+}
 void setup() {
-  gtx.initializationSystem();
+  gfx.initializationSystem();
 }
 
 void loop() {
-  gtx.render(readDataJoy);
+  gfx.render(gamePong, 10);
 }
